e99ccb55f7eec6345bcd58e1c8acc5af48451578
Mantas Lingaitis <mantasling@gmail.com>
2022-12-05 21:37:55 +0200

    >>> Advanced control flow >> Ternary >> Switch

---
+++ scripts/advancedControlFlow.js
@@ -0,0 +1,30 @@
+// ternary operator
+sectionNameSeparator("Ternary operator")
+
+var boolean = true;
+var experiencePoints = boolean ? "Statement is true" : "Statement is FALSE";
+
+console.log(experiencePoints);
+
+// Switch
+sectionNameSeparator("Switch")
+
+var cardinal_direction = "W";
+
+switch(cardinal_direction){
+    case "N":
+        console.log("North");
+        break;
+    case "S":
+        console.log("South");
+        break;
+    case "W":
+        console.log("West");
+        break;
+    case "E":
+        console.log("East");
+        break;
+    default:
+        console.log("Not a cardinal direction");
+        break;
+}
